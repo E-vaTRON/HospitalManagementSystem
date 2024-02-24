dbdf2d7e2ec502c4cff240ad6a4275ebb35f435f
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-02-24 19:13:29 +0700

    add DataProvider Tests

--- src/Infrastructure/DataProvider/HospitalManagementSystem.DataBaseContextProvider/Context/HospitalManagementSystemDbContext.cs
+++ src/Infrastructure/DataProvider/HospitalManagementSystem.DataBaseContextProvider/Context/HospitalManagementSystemDbContext.cs
@@ -4,25 +4,36 @@ public class HospitalManagementSystemDbContext : DbContext
 {
     public HospitalManagementSystemDbContext(DbContextOptions<HospitalManagementSystemDbContext> options) : base(options) { }
 
-    public virtual DbSet<AnalyzationTest>               AnalyzationTests        { get; set; } = null!;
-    public virtual DbSet<DeviceService>                 DeviceServices          { get; set; } = null!;
-    public virtual DbSet<Doctor>                        Doctors                 { get; set; } = null!;
-    public virtual DbSet<Employee>                      Employees               { get; set; } = null!;
-    public virtual DbSet<Drug>                          Drugs                   { get; set; } = null!;
-    public virtual DbSet<PatientTransactionHistory>     TransactionHistorys     { get; set; } = null!;
-    public virtual DbSet<GoodsImportation>              GetGoodsImportations    { get; set; } = null!;
-    public virtual DbSet<HistoryMedicalExam>            HistoryMedicalExams     { get; set; } = null!;
-    public virtual DbSet<Inventory>                     Inventories             { get; set; } = null!;
-    public virtual DbSet<Patient>                       Patients                { get; set; } = null!;
-    public virtual DbSet<Suppling>                      Supplings               { get; set; } = null!;
-    public virtual DbSet<Storage>                       Storages                { get; set; } = null!;
-    public virtual DbSet<Bill>                          Bills                   { get; set; } = null!;
-    //public virtual DbSet<User>                          Users                   { get; set; } = null!;
-    //public virtual DbSet<Role>                          Roles                   { get; set; } = null!;
-
-
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
     }
+
+    public virtual DbSet<Alert>                 Alerts                  { get; set; } = null!;
+    public virtual DbSet<Department>            Departments             { get; set; } = null!;
+    public virtual DbSet<Room>                  Rooms                   { get; set; } = null!;
+    public virtual DbSet<RoomAllocation>        RoomAllocations         { get; set; } = null!;
+    public virtual DbSet<RoomAssignment>        RoomAssignments         { get; set; } = null!;
+    public virtual DbSet<Drug>                  Drugs                   { get; set; } = null!;
+    public virtual DbSet<DrugInventory>         DrugInventorys          { get; set; } = null!;
+    public virtual DbSet<Storage>               Storages                { get; set; } = null!;
+    public virtual DbSet<Suppling>              Supplings               { get; set; } = null!;
+    public virtual DbSet<GoodsImportation>      GoodsImportations       { get; set; } = null!;
+    public virtual DbSet<DeviceInventory>       DeviceInventorys        { get; set; } = null!;
+    public virtual DbSet<Diagnosis>             Diagnosises             { get; set; } = null!;
+    public virtual DbSet<DiagnosisSuggestion>   DiagnosisSuggestions    { get; set; } = null!;
+    public virtual DbSet<ICD>                   ICDs                    { get; set; } = null!;
+    public virtual DbSet<ICDD>                  ICDDs                   { get; set; } = null!;
+    public virtual DbSet<MedicalExam>           MedicalExams            { get; set; } = null!;
+    public virtual DbSet<MedicalExamEposode>    MedicalExamEposodes     { get; set; } = null!;
+    public virtual DbSet<Treatment>             Treatments              { get; set; } = null!;
+    public virtual DbSet<DeviceService>         DeviceServices          { get; set; } = null!;
+    public virtual DbSet<MedicalDevice>         MedicalDevices          { get; set; } = null!;
+    public virtual DbSet<Service>               Services                { get; set; } = null!;
+    public virtual DbSet<AnalysisTest>          AnalysisTests           { get; set; } = null!;
+    public virtual DbSet<Bill>                  Bills                   { get; set; } = null!;
+    public virtual DbSet<DrugBillDetail>        DrugBillDetails         { get; set; } = null!;
+    public virtual DbSet<Transaction>           Transactions            { get; set; } = null!;
+    //public virtual DbSet<User>                          Users                   { get; set; } = null!;
+    //public virtual DbSet<Role>                          Roles                   { get; set; } = null!;
 }
