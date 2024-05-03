6ed7b84ec580e314a20b48fbf4e8339560561ff6
Nguyễn Hoàng Phi Long <envaroy@gmail.com>
2024-05-03 08:34:11 +0700

    Service Provider

--- src/Infrastructure/ServiceProvider/HospitalManagementSystem.ServiceProvider/Providers/BaseProvider/Implementation/ServiceProviderBase.cs
+++ src/Infrastructure/ServiceProvider/HospitalManagementSystem.ServiceProvider/Providers/BaseProvider/Implementation/ServiceProviderBase.cs
@@ -1,64 +1,69 @@
 ﻿namespace HospitalManagementSystem.ServiceProvider;
 
-public abstract class ServiceProviderBase<TEntity, TEId> : IServiceProviderBase<TEntity, TEId>
+public abstract class ServiceProviderBase<TEntity, TEId, TDataProvider> : IServiceProviderBase<TEntity, TEId>
     where TEntity : class, IEntity<TEId>
+    where TDataProvider : IDataProviderBase<TEntity, TEId>
 {
-    public ServiceProviderBase()
+    protected TDataProvider DataProvider;
+
+    public ServiceProviderBase(TDataProvider dataProvider)
     {
+        this.DataProvider = dataProvider;
     }
 
     public Task<IEnumerable<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.FindAllAsync(predicate, cancellationToken);
     }
 
     public Task<TEntity?> FindByIdAsync(TEId id, CancellationToken cancellationToken = default, bool isQuickFind = true)
     {
-        throw new NotImplementedException();
+        return DataProvider.FindByIdAsync(id, cancellationToken, isQuickFind);
     }
 
     public Task AddAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.AddAsync(entity, cancellationToken);
     }
 
     public Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.AddRangeAsync(entities, cancellationToken);
     }
 
     public Task AddRangeAsync(CancellationToken cancellationToken = default, params TEntity[] entities)
     {
-        throw new NotImplementedException();
+        return DataProvider.AddRangeAsync(cancellationToken, entities);
     }
 
     public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.UpdateAsync(entity, cancellationToken);
     }
 
     public Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.DeleteAsync(entity, cancellationToken);
     }
 
     public Task DeleteByIdAsync(TEId id, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.DeleteByIdAsync(id, cancellationToken);
     }
 
     public Task DeleteRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return DataProvider.DeleteRangeAsync(entities, cancellationToken);
     }
 
     public Task DeleteRangeAsync(CancellationToken cancellationToken = default, params TEntity[] entities)
     {
-        throw new NotImplementedException();
+        return DataProvider.DeleteRangeAsync(entities, cancellationToken);
     }
 }
 
-public abstract class ServiceProviderBase<TEntity> : ServiceProviderBase<TEntity, string>
+public abstract class ServiceProviderBase<TEntity>(IDataProviderBase<TEntity, string> dataProvider) 
+    : ServiceProviderBase<TEntity, string, IDataProviderBase<TEntity, string>>(dataProvider)
     where TEntity : class, Domain.IEntity<string>
 {
 }
